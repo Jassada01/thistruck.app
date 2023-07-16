7c1256896198180a58d39a1e4a9136d06f8a9550
Jassada01 <jassada01@gmail.com>
2023-07-16 16:51:46 +0700

    update 20230716

--- lineProcess/autoreply - Copy.php
+++ lineProcess/autoreply - Copy.php
@@ -1,7 +1,7 @@
 <?php
 
 $channelAccessToken = '69Wn4DFUg+BnsFmiDlC/LNar3NarEaHIAOvQIKqKIeseUjTbcnpI0e3hhstuydCG2Jr/kAv5ce9io0eXXC7zqx8NFDV12jbNcgtF/gW1NF4HtkyPNYOJo3QuNmS/ZZUFfaSq2i9OeiqXMOSg6U7Y0QdB04t89/1O/w1cDnyilFU=';
-$companyGroupID = "Ca820e972e675bf390f6cb51b271f8ef5";
+
 $request = file_get_contents('php://input');
 $events = json_decode($request, true);
 
@@ -27,17 +27,6 @@ foreach ($events['events'] as $event) {
             else {
                 // Do nothing
                 //replyTextMessage($replyToken, "นี่เป็นช่องทางสำหรับการส่งข้อมูลแจ้งเตือนเท่านั้น ถ้าคุณมีคำถามหรือต้องการข้อมูลเพิ่มเติม, กรุณาติดต่อเราที่หมายเลข 063-914-4536 นะคะ");
-            }
-        }
-        else if ($messageType == 'image') {
-            $groupId = $event['source']['groupId'];
-            if ($groupId == $companyGroupID) {
-                $userId = $event['source']['userId'];
-                $messageId = $event['message']['id'];
-                $createDate = date('Y-m-d H:i:s');
-                $fileType = 'image';
-
-                insertLineAttachedFile($groupId, $userId, $messageId, $createDate, $fileType);
             }
         }
     }
@@ -73,36 +62,4 @@ function replyTextMessage($replyToken, $text)
 
     $context = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-}
-
-function insertLineAttachedFile($groupId, $userId, $messageId, $createDate, $fileType)
-{
-    $server_name = "localhost";
-    $UID = "root";
-    $Pass = "}Ww]3v2CX<2mSH$7";
-    $DB_name = "mysystem";
-    $conn = new mysqli($server_name,$UID,$Pass,$DB_name);
-    
-    // Setting for support Thai
-    mysqli_set_charset($conn, "utf8");
-
-    // Check connection
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    }
-
-    // Prepare and bind the SQL statement
-    $stmt = $conn->prepare('INSERT INTO line_attached_file (group_id, user_id, message_id, create_date, file_type) VALUES (?, ?, ?, ?, ?)');
-    $stmt->bind_param('sssss', $groupId, $userId, $messageId, $createDate, $fileType);
-
-    // Execute the SQL statement
-    $stmt->execute();
-
-    // Close the statement and connection
-    $stmt->close();
-    $conn->close();
-
-    // Call PythonScript
-    $pythonCommand = ' python3 /var/www/thistruck.app/lineProcess/loadfile.py '.$messageId;
-    shell_exec($pythonCommand);
 }
---
+++ lineProcess/loadfile.py
@@ -0,0 +1,81 @@
+import os
+import random
+import string
+import requests
+from datetime import datetime
+import pymysql
+
+def generate_random_filename():
+    letters = string.ascii_lowercase
+    random_filename = ''.join(random.choice(letters) for i in range(10))
+    return random_filename
+
+def download_image(message_id):
+    access_token = '69Wn4DFUg+BnsFmiDlC/LNar3NarEaHIAOvQIKqKIeseUjTbcnpI0e3hhstuydCG2Jr/kAv5ce9io0eXXC7zqx8NFDV12jbNcgtF/gW1NF4HtkyPNYOJo3QuNmS/ZZUFfaSq2i9OeiqXMOSg6U7Y0QdB04t89/1O/w1cDnyilFU='
+    url = f'https://api-data.line.me/v2/bot/message/{message_id}/content'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        random_filename = generate_random_filename()
+        filename = f'{random_filename}.jpg'
+        file_path = f'/var/www/thistruck.app/assets/media/uploadfile/linemedia/{filename}'
+        
+
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+
+        return file_path, filename
+    else:
+        return None, None
+
+def update_line_attached_file(message_id, download_date, path, file_name):
+    db_host = 'localhost'
+    db_user = 'root'
+    db_password = '}Ww]3v2CX<2mSH$7'
+    db_database = 'mysystem'
+
+    connection = pymysql.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_database
+    )
+    cursor = connection.cursor()
+
+    update_query = '''
+    UPDATE line_attached_file 
+    SET download_date = %s, path = %s, file_name = %s 
+    WHERE message_id = %s
+    '''
+
+    values = (download_date, path, file_name, message_id)
+    cursor.execute(update_query, values)
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+def main(message_id):
+    download_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    path, file_name = download_image(message_id)
+    webpath = f'assets/media/uploadfile/linemedia/{file_name}'
+    if path and file_name:
+        update_line_attached_file(message_id, download_date, webpath, file_name)
+        print('Data updated successfully.')
+    else:
+        print('Failed to download the image.')
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print('Please provide the message ID as a command line argument.')
+        sys.exit(1)
+
+    message_id = sys.argv[1]
+    main(message_id)
